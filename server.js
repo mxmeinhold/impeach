ed6936278d274b7f91bd9c05c0ff67cccf633f94
Max Meinhold <mxmeinhold@gmail.com>
2020-06-18 21:15:14 -0400

    Standardise alerts (and reformat server.js)

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.DB_URI);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  const evalSchema = mongoose.Schema({ // eslint-disable-line new-cap
+db.once('open', function () {
+  const evalSchema = mongoose.Schema({
     name: String,
     eboard: [String],
     likes: String,
@@ -23,29 +23,34 @@ db.once('open', function() {
 // Configure the OpenID Connect strategy for use by Passport.
 const passport = require('passport');
 const Strategy = require('passport-openidconnect').Strategy;
-passport.use(new Strategy({
-  issuer: 'https://sso.csh.rit.edu/auth/realms/csh',
-  authorizationURL: 'https://sso.csh.rit.edu/auth/realms/csh/protocol/openid-connect/auth',
-  tokenURL: 'https://sso.csh.rit.edu/auth/realms/csh/protocol/openid-connect/token',
-  userInfoURL: 'https://sso.csh.rit.edu/auth/realms/csh/protocol/openid-connect/userinfo',
-  clientID: process.env.CLIENT_ID,
-  clientSecret: process.env.CLIENT_SECRET,
-  callbackURL: 'http://' + process.env.HOST + '/login/callback',
-},
-function(accessToken, refreshToken, profile, cb) {
-  return cb(null, profile);
-}));
-
+passport.use(
+  new Strategy(
+    {
+      issuer: 'https://sso.csh.rit.edu/auth/realms/csh',
+      authorizationURL:
+        'https://sso.csh.rit.edu/auth/realms/csh/protocol/openid-connect/auth',
+      tokenURL:
+        'https://sso.csh.rit.edu/auth/realms/csh/protocol/openid-connect/token',
+      userInfoURL:
+        'https://sso.csh.rit.edu/auth/realms/csh/protocol/openid-connect/userinfo',
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: 'http://' + process.env.HOST + '/login/callback',
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      return cb(null, profile);
+    }
+  )
+);
 
 // Configure Passport authenticated session persistence.
-passport.serializeUser(function(user, cb) {
+passport.serializeUser(function (user, cb) {
   cb(null, user);
 });
 
-passport.deserializeUser(function(obj, cb) {
+passport.deserializeUser(function (obj, cb) {
   cb(null, obj);
 });
-
 
 // Create a new Express application.
 const express = require('express');
@@ -53,31 +58,38 @@ const app = express();
 
 // Configure body parsing
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({
-  extended: true,
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(bodyParser.json());
 
 // Configure session handling
-app.use(require('express-session')({
-  secret: process.env.EXPRESS_SESSION_SECRET,
-  resave: true,
-  saveUninitialized: true,
-}));
+app.use(
+  require('express-session')({
+    secret: process.env.EXPRESS_SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
 // Initialize Passport and restore authentication state from the session.
 app.use(passport.initialize());
 app.use(passport.session());
 
 // Authentication: authenticates with CSH OIDC and returns to origin point
-app.get('/login',
-    passport.authenticate('openidconnect'));
+app.get('/login', passport.authenticate('openidconnect'));
 
-app.get('/login/callback',
-    passport.authenticate('openidconnect', {failureRedirect: '/login'}),
-    function(req, res) {
-      res.redirect(req.session.returnTo);
-    });
+app.get(
+  '/login/callback',
+  passport.authenticate('openidconnect', {
+    failureRedirect: '/login',
+  }),
+  function (req, res) {
+    res.redirect(req.session.returnTo);
+  }
+);
 
 // Require login
 app.use(require('connect-ensure-login').ensureLoggedIn());
@@ -86,7 +98,7 @@ app.use(require('connect-ensure-login').ensureLoggedIn());
 const git = require('git-rev');
 let rev = 'GitHub';
 let gitUrl = 'https://github.com/mxmeinhold/impeach';
-git.short(function(commit) {
+git.short(function (commit) {
   gitUrl = gitUrl + '/tree/' + commit;
   rev = commit;
 });
@@ -112,20 +124,66 @@ const eboard = {
   adhoc: 'Ad Hoc',
 };
 
-app.get('/', function(req, res) {
-  res.render('index', {gitUrl: gitUrl, gitRev: rev, eboard: eboard, error: false});
+app.get('/', function (req, res) {
+  res.render('index', {
+    gitUrl: gitUrl,
+    gitRev: rev,
+    eboard: eboard,
+    alerts: [],
+  });
 });
 
-app.post('/', function(req, res) {
-  const submission = new Open(req.body);
-  console.log(submission);
-  let error = false;
-  submission.save((err, submission) => {
-    error = err;
+const body2openEval = (body) => {
+  const submission = new Open({
+    name: (body.name && body.name.trim()) || '',
+    eboard: body.eboard,
+    likes: (body.likes && body.likes.trim()) || '',
+    dislikes: (body.dislikes && body.dislikes.trim()) || '',
+    comments: (body.comments && body.comments.trim()) || '',
   });
-  // error=true;
-  // res.render('submit', {gitUrl: gitUrl, gitRev: rev, error: error});
-  res.render('index', {gitUrl: gitUrl, gitRev: rev, eboard: eboard, error: error, submitted: !error});
+  if (submission.likes || submission.dislikes || submission.comments) {
+    return submission;
+  }
+};
+
+app.post('/', function (req, res) {
+  const alerts = [];
+  const submission = body2openEval(req.body);
+  if (!submission) {
+    alerts.push({
+      message: 'Empty submission, eval not recorded',
+      attributes: {
+        class: 'alert-warning',
+        role: 'alert',
+      },
+    });
+  } else {
+    submission.save((err, submission) => {
+      if (err) {
+        alerts.push({
+          message: 'Something went wrong, sorry. Eval not submitted',
+          attributes: {
+            class: 'alert-danger',
+            role: 'alert',
+          },
+        });
+      } else {
+        alerts.push({
+          message: 'Eval Submitted',
+          attributes: {
+            class: 'alert-danger',
+            role: 'alert',
+          },
+        });
+      }
+    });
+  }
+  res.render('index', {
+    gitUrl: gitUrl,
+    gitRev: rev,
+    eboard: eboard,
+    alerts: alerts,
+  });
 });
 
 app.listen(process.env.PORT);
